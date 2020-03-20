3b92341557a7957de96f03adc746d3bf1f4a4aed
lingmk <mingkun.ling@qq.com>
2020-03-21 01:16:43 +0800

    get_logger用日志logging包实现

--- utils/tools.py
+++ utils/tools.py
@@ -1,4 +1,5 @@
 import os
+import logging
 import random
 import time
 import torch
@@ -15,42 +16,53 @@ def now_str():
     return time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime())
 
 
-def epoch_timer(func):
-    """
-    装饰器。epoch计时器，记录一个epoch用时并打印
-    :param func: 被装饰函数，是epoch_train
-    :return:
-    """
+def timer(logger):
+    def timer_func(func):
+        """
+        装饰器。epoch计时器，记录一个epoch用时并打印
+        :param func: 被装饰函数，是epoch_train
+        :return:
+        """
 
-    def timer(*args, **kwargs):  # func的所有入参
-        begin_time = datetime.now()  # 开始时间
-        res = func(*args, **kwargs)  # 执行func，记录func返回值
-        end_time = datetime.now()  # 结束时间
-        mm, ss = divmod((end_time - begin_time).seconds, 60)  # 秒换算成分、秒
-        hh, mm = divmod(mm, 60)  # 分钟换算成时、分
-        duration_str = 'Duration: {:02d}:{:02d}:{:02d}|'.format(hh, mm, ss)  # HH:mm:ss
-        log(duration_str)  # 记录到日志里面
-        return res  # 返回func返回值
+        def timer_logger(*args, **kwargs):  # func的所有入参
+            begin_time = datetime.now()  # 开始时间
+            res = func(*args, **kwargs)  # 执行func，记录func返回值
+            end_time = datetime.now()  # 结束时间
+            mm, ss = divmod((end_time - begin_time).seconds, 60)  # 秒换算成分、秒
+            hh, mm = divmod(mm, 60)  # 分钟换算成时、分
+            duration_str = 'Duration: {:02d}:{:02d}:{:02d}|'.format(hh, mm, ss)  # HH:mm:ss
+            logger.info(duration_str)  # 记录到日志里面
+            return res  # 返回func返回值
 
-    return timer
+        return timer_logger
 
-
-def log(msg, logfile=Config.LOG_FILE):
-    """
-    日志记录
-    :param msg: 要记录的内容
-    :param logfile: 日志文件，如果没有就创建
-    :return:
-    """
-    log_f = open(logfile, mode='a')
-    log_f.write(msg)  # 写到日志
-    log_f.flush()  # 很有必要的马上写
-    log_f.close()
-    print(msg, end='', flush=True)  # 马上打印到终端
-    pass
+    return timer_func
 
 
-def save_weight(net, name, epoch, save_dir=Config.WEIGHT_SAVE_PATH):
+def get_logger(logger_type='train'):
+    assert logger_type in ('train', 'test')
+
+    logger = logging.getLogger(logger_type)
+    if not logger.handlers:  # 防止加入多个handler造成重复写日志
+        fmt = logging.Formatter('%(asctime)s %(msg)s')  # 日志格式
+
+        proj_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        logfile = os.path.join(os.path.join(proj_root, 'res'),
+                               '{:s}.log'.format(logger_type))  # 日志位置
+
+        fh = logging.FileHandler(logfile)
+        fh.setFormatter(fmt)
+        logger.addHandler(fh)  # 输出到文件
+
+        sh = logging.StreamHandler()
+        sh.setFormatter(fmt)
+        logger.addHandler(sh)  # 输出到终端
+
+    logger.setLevel(logging.DEBUG)
+    return logger
+
+
+def save_weight(net, name, epoch, save_dir=None):
     """
     保存模型参数，模型参数文件名格式 {模型名}-{保存日期时间}-epoch-{第几个epoch}.pkl
     :param net: 要保存参数的模型
@@ -59,6 +71,9 @@ def save_weight(net, name, epoch, save_dir=Config.WEIGHT_SAVE_PATH):
     :param save_dir: 保存模型参数的路径，不包含文件名
     :return:
     """
+    if save_dir is None or not os.path.exists(save_dir):
+        proj_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        save_dir = os.path.join(proj_root, 'res')
     filename = '{:s}-{:s}-epoch-{:02d}.pth'
     filename = filename.format(name, now_str(), epoch)  # 模型参数文件{模型名}-{保存日期时间}-epoch-{第几个epoch}.pkl
     save_dir = os.path.join(save_dir, filename)  # 保存的文件绝对路径
@@ -156,13 +171,13 @@ if __name__ == '__main__':
     print('precision', get_metrics(c, metrics='precision'))
 
 
-    @epoch_timer
+    @timer(get_logger())
     def test_train():  # 模拟一个epoch训练
         time.sleep(random.randint(0, 5) / 5)
         return random.random(), random.random()
 
 
-    @epoch_timer
+    @timer(get_logger())
     def test_valid():  # 模拟一个epoch验证
         time.sleep(random.randint(0, 5) / 5)
         return random.random(), random.random()
@@ -170,18 +185,16 @@ if __name__ == '__main__':
 
     epochs = 5
     for e in range(1, epochs + 1):
-        epoch_str = '{:s}|Epoch: {:02d}|'.format(str(datetime.now()), e)
-        log(epoch_str, './test.log')
+        get_logger().info('Epoch: {:02d}'.format(e))
 
         # 模拟一个epoch训练
         t_loss, t_miou = test_train()
         train_str = 'Train Loss: {:.4f}|Train mIoU: {:.4f}|'.format(t_loss, t_miou)
-        log(train_str, './test.log')
+        get_logger().info(train_str)
 
         # 模拟一个epoch验证
         v_loss, v_miou = test_valid()
         valid_str = 'Valid Loss: {:.4f}|Valid mIoU: {:.4f}|'.format(v_loss, v_miou)
-        log(valid_str, './test.log')
-        log('\n', './test.log')
+        get_logger().info(valid_str)
         pass
     pass
